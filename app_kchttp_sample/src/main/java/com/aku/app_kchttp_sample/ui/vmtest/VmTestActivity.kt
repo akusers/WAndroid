3eea8157ecd3dbcda230f70352fdf7244abd0b12
Zsc <732116857@qq.com>
2020-01-19 16:49:03 +0800

    fix load data

--- app_kchttp_sample/src/main/java/com/aku/app_kchttp_sample/ui/vmtest/VmTestActivity.kt
+++ app_kchttp_sample/src/main/java/com/aku/app_kchttp_sample/ui/vmtest/VmTestActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.aku.aac.core.BaseActivity
+import com.aku.aac.kchttp.ext.doError
+import com.aku.aac.kchttp.ext.doSuccess
 import com.aku.app_kchttp_sample.R
 import com.aku.common.util.toJson
+import com.blankj.utilcode.util.ToastUtils
 import kotlinx.android.synthetic.main.vm_test_act.*
 
 class VmTestActivity : BaseActivity() {
@@ -16,8 +19,13 @@ class VmTestActivity : BaseActivity() {
 
         val viewModel = ViewModelProvider(this)
             .get(VmTestViewModel::class.java)
-        viewModel.listBanner.observe(this, Observer{
-            tvContent.text=it.toJson()
+        viewModel.listBanner.observe(this, Observer {
+            it.doSuccess { list ->
+                tvContent.text = list.toJson()
+            }.doError { error ->
+                ToastUtils.showShort(error.toJson())
+            }
+
         })
         viewModel.loadBanner()
         //这里可以直接绑定一个显示的dialog
--- app_kchttp_sample/src/main/java/com/aku/app_kchttp_sample/ui/vmtest/VmTestViewModel.kt
+++ app_kchttp_sample/src/main/java/com/aku/app_kchttp_sample/ui/vmtest/VmTestViewModel.kt
@@ -3,6 +3,7 @@ package com.aku.app_kchttp_sample.ui.vmtest
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.aku.aac.kchttp.data.BaseResult
 import com.aku.aac.kchttp.ext.catchResult
 import com.aku.aac.kchttp.ext.doError
 import com.aku.aac.kchttp.ext.doSuccess
@@ -15,19 +16,12 @@ import kotlinx.coroutines.launch
 
 class VmTestViewModel : ViewModel() {
 
-    val listBanner = MutableLiveData<List<Banner>>()
+    val listBanner = MutableLiveData<BaseResult<List<Banner>>>()
 
     fun loadBanner():Job {
        return viewModelScope.launch(Dispatchers.IO) {
-            catchResult { api.getBanner() }
-                .doSuccess {
-                    listBanner.postValue(it)
-                }.doError {
-                    ToastUtils.showShort(it.msg)
-                }
+           listBanner.postValue(catchResult { api.getBanner() })
         }
-
-
     }
 
 
